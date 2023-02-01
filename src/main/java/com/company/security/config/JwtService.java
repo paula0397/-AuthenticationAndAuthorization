f4d7a86e7cf74b9279117e82a55345f94a3031e2
Paula0397 <paula230397@gmail.com>
2023-02-01 13:17:08 -0500

    config security

--- src/main/java/com/company/security/config/JwtService.java
+++ src/main/java/com/company/security/config/JwtService.java
@@ -51,6 +51,11 @@ public class JwtService {
     }
 
     private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token){
